9d714033a233184d230891a9de61d539329573e7
bogdan <bogycoins@gmail.com>
2021-05-20 20:14:51 +0300

    db stuff

--- pages/api/wallet.js
+++ pages/api/wallet.js
@@ -10,13 +10,68 @@ import {fetchWalletTokens} from '/pages/api/external/covalenthq'
 const wallet = '0x8330D58cC6458a1579F11f68E466829f1d19c78c';
 const chainId = 56;
 
-async function writeDb(data){
+const User = db.User;
+const Snapshot = db.Snapshot;
+
+async function createUser(data){
   try {
-    await db.sequelize.authenticate();
-    console.log('Connection has been established successfully.');
+    // Check if user with that email if already exists
+    const name = "bogdan";
+    const email = "bogycoins@gmail.com";
+
+    const user = await User.findOne({
+      where: { address: wallet }
+    })
+    if(user){
+      // return res.status(422).send(`User already exist with that ${email}`)
+    }
+    const newUser = await User.create({
+      name,
+      email,
+      address: wallet
+    })
+    console.log(newUser)
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error(error)
   }
+}
+
+async function saveSnapshot(data) {
+  const user = "bogdan";
+
+  try {
+    const time = new Date().toISOString();
+
+    let date = formatDate(time);
+    const newSnapshot = await Snapshot.create({
+      time: date,
+      user,
+      wallet: data
+    })
+    // console.log(newSnapshot)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+async function showToken(){
+  const token = "BNB";
+  const user = "bogdan";
+
+  const snapshots = await Snapshot.findAll({
+    where: { user }
+  });
+
+  const list = [];
+  if (snapshots){
+    snapshots.forEach(entry => {
+      const row = entry.getDataValue('wallet');
+      const coin = row['BNB'];
+      console.log("BNB: ", coin)
+      list.push(coin);
+    })
+  }
+  return list;
 }
 
 function relevantData(wallet) {
@@ -34,14 +89,36 @@ function relevantData(wallet) {
 
   return tokens;
 }
+
 export default async (req, res) => {
 
   const text = await fetchWalletTokens(wallet, chainId);
   const parsed = relevantData(text);
 
-  res.status(200).json(parsed);
+  //make sure db is working
+  try {
+    await db.sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 
-  //inregistreaza in db
-  writeDb(parsed);
+  //inregistreaza user in db
+  createUser();
+
+  //salveaza un snapshot
+  saveSnapshot(parsed);
+
+  //afiseaza evolutia la un token
+   const toshow = await showToken();
+
+  // res.status(200).json(parsed);
+  res.status(200).json(toshow);
 }
 
+function formatDate(s) {
+  let b = s.split(/\D/);
+  return b[0] + '-' + b[1] + '-' + b[2] + ' ' +
+      b[3] + ':' + b[4] + ':' + b[5] + '.' +
+      b[6].substr(0,3);// + '+00:00';
+}
